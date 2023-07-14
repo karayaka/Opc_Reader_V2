da494077e3e3941eb944e3e343c6c0aa858710c3
Beril BAHAR <beril@etkinbilgi.com>
2023-07-14 12:13:45 +0300

    View models

---
+++ Opc_Reader_V2/ViewModels/RunPartViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opc_Reader_V2.ViewModels
+{
+    public class RunPartViewModel
+    {
+        public int ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Guid JobGuid { get; set; }
+        public string JobName { get; set; }
+
+        public Guid PlanGuid { get; set; }
+        public string PlanName { get; set; }
+
+        public Guid RunGuid { get; set; }
+        public string RunNumber { get; set; }
+
+        public double ActualCutTime { get; set; }
+
+        public double ActualStopTime { get; set; }
+
+        public double ActualWaitTime { get; set; }
+
+        public int PartId { get; set; }
+
+        public int PartRefId { get; set; }
+
+        public int PartNumber { get; set; }
+
+        public int CutState { get; set; }
+
+        public DateTime CutStartTime { get; set; }
+
+        public DateTime CutEndTime { get; set; }
+
+        public int SortState { get; set; }
+
+        public DateTime SortStartTime { get; set; }
+
+        public DateTime SortEndTime { get; set; }
+
+        public int StackAreaType { get; set; }
+
+        public string PartName { get; set; }
+    }
+}
